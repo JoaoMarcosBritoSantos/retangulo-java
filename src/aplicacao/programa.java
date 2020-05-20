e327415effd2ec4f03a90f3bd83265933e3444d4
JoaoMarcosBritoSantos <65055414+JoaoMarcosBritoSantos@users.noreply.github.com>
2020-05-20 20:22:16 -0300

    Leitura da altura com validacao

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -7,7 +7,7 @@ public class programa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 
-		double largura;
+		double largura,altura;
 		
 		System.out.print("Gigite a largura do retângulo: ");
 		largura = sc.nextDouble();
@@ -16,6 +16,15 @@ public class programa {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 		largura = sc.nextDouble();
 		}
+		
+		System.out.print("Digite a altura do retângulo: ");
+		altura = sc.nextDouble();
+		
+		while (altura <= 0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+		altura = sc.nextDouble();
+		}
+		
 		
 		
 		
